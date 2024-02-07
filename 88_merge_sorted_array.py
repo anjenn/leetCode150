6d840eba06f22de56c390e221f2102c96a35749f
anjenn <anjenn9912@gmail.com>
2024-02-07 21:08:52 +0100

    Update 88_merge_sorted_array.py
    hints given

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -5,6 +5,8 @@
 # To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
 # and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
+# Begin by setting pointers at the end of the meaningful elements in both arrays (m-1 for nums1 and n-1 for nums2) and another pointer at the end of nums1 (m+n-1) to indicate the current position to insert the next greatest element.Compare elements from nums1 and nums2 from the end, moving the greater element into the current position in nums1 and adjusting pointers accordingly.Ensure that if nums2 still has elements left after nums1 is exhausted, they are copied over to the beginning of nums1.
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
@@ -12,6 +14,9 @@ class Solution:
         """
         if len(nums1) == 0:
             return
+        pointer1 = nums1[m-1]
+        porinter2 = nums2[]n-1]
+        porinter3 = nums1[m+n-1] # do sth about it
 
         for i in range(n-1):
             if nums2[i] >= nums1[i] and nums2[i] <= nums1[i+1]:
